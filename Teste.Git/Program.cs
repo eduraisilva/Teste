fcb78f12e565f837062553cab1327337383aa309
Eduardo Silva <eduraisilva@gmail.com>
2020-02-21 22:06:50 -0300

    Projeto Iniciado

---
+++ Teste.Git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Teste.Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            var nome = Console.ReadLine();
+
+            if (nome == "Eduardo") {
+                nome = "Eduardo lindo e Maravilhoso";
+            }
+
+            Console.WriteLine($"Welcome {nome}! Nice to meet you!!!");
+        }
+    }
+}
